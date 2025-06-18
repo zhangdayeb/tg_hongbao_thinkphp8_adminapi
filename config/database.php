4644f40ab557138331277439c491113ed5860227
zhangdayeb <you@example.com>
2025-06-18 15:56:44 +0800

    'up'

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
             // 服务器地址
             'hostname'        => env('DB_HOST', '127.0.0.1'),
             // 数据库名
-            'database'        => env('DB_NAME', 'ntp_im'),
+            'database'        => env('DB_NAME', 'ntp_feitou'),
             // 用户名
             'username'        => env('DB_USER', 'root'),
             // 密码
